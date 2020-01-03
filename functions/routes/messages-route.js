0ee3291d96d55dbb36389dc2f2dd70848845f72f
Eedoh <kabelly@gmail.com>
2020-01-03 18:11:55 +0200

    feat(messages): added logic

--- functions/routes/messages-route.js
+++ functions/routes/messages-route.js
@@ -1,9 +1,20 @@
 const Router = require('express').Router();
 const messagesWrap = require('../wrappers/messages-wrap');
-const { resHandler } = require('../middlewares/app-middlewares');
+const { resHandler, verifyParams } = require('../middlewares/app-middlewares');
+const { verifyAdmin } = require('../middlewares/auth');
 
 Router.get('/', messagesWrap.fetchMessages, resHandler);
 
-Router.get('/:first/:last', messagesWrap.fetchMessages, resHandler);
+Router.get('/:messageId', messagesWrap.fetchMessage, resHandler);
+
+Router.get('/paging/:first/:last', messagesWrap.fetchMessages, resHandler);
+
+Router.all('*', verifyAdmin);
+
+Router.post('/', messagesWrap.createMessage, resHandler);
+
+Router.put('/:messageId', verifyParams, messagesWrap.updateMessage, resHandler);
+
+Router.delete('/:messageId', verifyParams, messagesWrap.deleteMessage, resHandler);
 
 module.exports = Router;
--- functions/wrappers/messages-wrap.js
+++ functions/wrappers/messages-wrap.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
 const { fetchSnapshot } = require('../util/app-util');
+const { errHandler } = require('../middlewares/app-middlewares');
 const db = admin.database();
 
 const fetchMessages = async (req, res, next) => {
@@ -7,6 +8,40 @@ const fetchMessages = async (req, res, next) => {
     next();
 }
 
+const fetchMessage = async (req, res, next) => {
+    req.data = await fetchSnapshot(req, db.ref(`/newsFeed/${req.params.messageId}`));
+    next();
+}
+
+const createMessage = async (req, res, next) => {
+    const message = db.ref('/newsFeed').push();
+    req.body.message.id = message.key;
+    message.set(req.body.message).then(() => {
+        req.data = { success: true };
+        return next();
+    }).catch(errHandler);
+}
+
+const updateMessage = (req, res, next) => {
+    const messageToUpdate = db.ref(`/newsFeed/${req.params.messageId}`);
+    messageToUpdate.set(req.body.message).then(() => {
+        req.data = { success: true };
+        return next();
+    }).catch(errHandler);
+}
+
+const deleteMessage =(req, res, next) => {
+    const messageToDelete = db.ref(`/newsFeed/${req.params.messageId}`);
+    messageToDelete.remove().then(() => {
+        req.data = { success: true };
+        return next();
+    }).catch(errHandler);
+}
+
 module.exports = {
-    fetchMessages
+    fetchMessages,
+    fetchMessage,
+    createMessage,
+    updateMessage,
+    deleteMessage
 }
